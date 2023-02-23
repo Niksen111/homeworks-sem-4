2b090f7650bdbbdcb457fba64c23253b2c42c278
Niksen111 <nikiksen2003@gmail.com>
2023-02-23 12:57:06 +0300

    Added tests

---
+++ 1Homework/1.2Fibonacci/Fibonacci/FibonacciTests/UnitTest1.fs
@@ -0,0 +1,18 @@
+module FibonacciTests
+
+open Fibonacci
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let FibonacciWorks () =
+    Assert.AreEqual(-1, FibonacciCalculation.Fibonacci(-666))
+    Assert.AreEqual(0, FibonacciCalculation.Fibonacci(0))
+    Assert.AreEqual(1, FibonacciCalculation.Fibonacci(1))
+    Assert.AreEqual(1, FibonacciCalculation.Fibonacci(2))
+    Assert.AreEqual(2, FibonacciCalculation.Fibonacci(3))
+    Assert.AreEqual(1597, FibonacciCalculation.Fibonacci(17))
+    Assert.AreEqual(102334155, FibonacciCalculation.Fibonacci(40))
