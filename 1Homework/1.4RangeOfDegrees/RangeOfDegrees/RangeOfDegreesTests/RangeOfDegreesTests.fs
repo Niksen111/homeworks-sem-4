92692223b21b57f29494c0dfbbfa1b5444b3e8d8
Niksen111 <nikiksen2003@gmail.com>
2023-02-23 15:31:35 +0300

    Fixed test name

--- 1Homework/1.4RangeOfDegrees/RangeOfDegrees/RangeOfDegreesTests/RangeOfDegreesTests.fs
+++ 1Homework/1.4RangeOfDegrees/RangeOfDegrees/RangeOfDegreesTests/RangeOfDegreesTests.fs
@@ -5,7 +5,7 @@ open RangeOfDegrees.RangeOfDegrees
 open NUnit.Framework
 
 [<Test>]
-let Test1 () =
+let RangeOfDegreesTests () =
     Assert.AreEqual([ 1.; 2.; 4. ], rangeOfDegreesOf2(0, 2))
     Assert.AreEqual([ 1024.; 2048.; 4096. ], rangeOfDegreesOf2(10, 2))
     Assert.AreEqual([ 64. ], rangeOfDegreesOf2(6, 0))
