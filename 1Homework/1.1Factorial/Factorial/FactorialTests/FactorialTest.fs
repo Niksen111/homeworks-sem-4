103fdda4a1a3ed3f65ecf33ac1e238a2bad20384
Niksen111 <nikiksen2003@gmail.com>
2023-02-23 12:10:48 +0300

    Writed interactive part

--- 1Homework/1.1Factorial/Factorial/FactorialTests/FactorialTest.fs
+++ 1Homework/1.1Factorial/Factorial/FactorialTests/FactorialTest.fs
@@ -4,7 +4,7 @@ open Factorial.Factorial
 open NUnit.Framework
 
 [<Test>]
-let Test1 () =
+let FactorialWorks () =
     Assert.AreEqual(0, Factorial(-1))
     Assert.AreEqual(1, Factorial(0))
     Assert.AreEqual(1, Factorial(1))
