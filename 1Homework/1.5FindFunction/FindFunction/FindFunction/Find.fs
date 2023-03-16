ffc2e5cb1a241e13948efc9adeb6d7a847a8bddd
Niksen111 <nikiksen2003@gmail.com>
2023-03-16 14:44:42 +0300

    if changed to match

--- 1Homework/1.5FindFunction/FindFunction/FindFunction/Find.fs
+++ 1Homework/1.5FindFunction/FindFunction/FindFunction/Find.fs
@@ -3,13 +3,9 @@
 module FindFunction =
     let find (element, list : List<'a>) =
         let rec realFind (element, myList : List<'a>, i) =
-            if myList = List.Empty then
-                i
-            else if myList.Head = element then
-                i + 1
-            else if myList.Tail = List<'a>.Empty then
-                -1
-            else
-                realFind (element, myList.Tail, i + 1)
+            match myList with
+            | [] -> -1
+            | head :: _ when head = element -> i
+            | _ :: tail -> realFind (element, tail, i + 1)
                 
-        realFind (element, list, -1)
+        realFind (element, list, 0)
