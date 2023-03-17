214a240ce2a6a62106fdbb1d3c392a236ea2fcdc
Niksen111 <nikiksen2003@gmail.com>
2023-03-17 21:42:15 +0300

    Added Tree type, writed TreeMap function

---
+++ 2Homework/2.1EvenNumbers/MapForTrees/MapForTrees/MapForTrees/TreeFunctinos.fs
@@ -0,0 +1,12 @@
+﻿namespace MapForTrees
+
+module BinaryTree =
+    type Tree<'a> =
+        | TreeFull of 'a * Tree<'a> * Tree<'a>
+        | TreeOne of 'a * Tree<'a>
+        | Tip of 'a
+    let rec TreeMap func tree =
+        match tree with
+        | TreeFull(v, l, r) -> TreeFull (func v, TreeMap func l, TreeMap func r)
+        | TreeOne(v, s) -> TreeOne (func v, TreeMap func s)
+        | Tip v -> Tip (func v)
