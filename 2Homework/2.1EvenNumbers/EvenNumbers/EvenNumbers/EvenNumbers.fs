94a22a39fd0d215636dccf29920411685de915df
Niksen111 <nikiksen2003@gmail.com>
2023-03-17 18:09:30 +0300

    Added FsTest and FsCheck tests

--- 2Homework/2.1EvenNumbers/EvenNumbers/EvenNumbers/EvenNumbers.fs
+++ 2Homework/2.1EvenNumbers/EvenNumbers/EvenNumbers/EvenNumbers.fs
@@ -2,10 +2,10 @@
 
 module EvenNumbers =
     let EvenNumbersFold list =
-        (0, list) ||> List.fold (fun s v -> s + ((v + 1) % 2) * v)
+        (0, list) ||> List.fold (fun s v -> s + abs((v + 1) % 2) * v)
         
     let EvenNumbersFilter list =
-        list |> List.filter (fun n -> n % 2 = 0) |> Seq.sum
+        list |> List.filter (fun n -> n % 2 = 0) |> List.sum
         
     let EvenNumbersMap list =
-        Seq.map (fun x -> ((x + 1) % 2) * x) list |> Seq.sum
+        list |> List.map (fun x -> abs((x + 1) % 2) * x) |> List.sum
