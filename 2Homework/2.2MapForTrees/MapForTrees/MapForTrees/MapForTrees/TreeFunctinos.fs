f0e9fb181aae4b6f06be92b1ceb9a5d61ec97e74
Niksen111 <nikiksen2003@gmail.com>
2023-03-17 22:05:28 +0300

    Project moved to right directory, added FsTests

--- 2Homework/2.2MapForTrees/MapForTrees/MapForTrees/MapForTrees/TreeFunctinos.fs
+++ 2Homework/2.2MapForTrees/MapForTrees/MapForTrees/MapForTrees/TreeFunctinos.fs
@@ -5,8 +5,8 @@ module BinaryTree =
         | TreeFull of 'a * Tree<'a> * Tree<'a>
         | TreeOne of 'a * Tree<'a>
         | Tip of 'a
-    let rec TreeMap func tree =
+    let rec treeMap func tree =
         match tree with
-        | TreeFull(v, l, r) -> TreeFull (func v, TreeMap func l, TreeMap func r)
-        | TreeOne(v, s) -> TreeOne (func v, TreeMap func s)
+        | TreeFull(v, l, r) -> TreeFull (func v, treeMap func l, treeMap func r)
+        | TreeOne(v, s) -> TreeOne (func v, treeMap func s)
         | Tip v -> Tip (func v)
