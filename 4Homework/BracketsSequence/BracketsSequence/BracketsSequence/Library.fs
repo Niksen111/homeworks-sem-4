00737dd845ba45b3e75602d5dfa1681407632127
niksen111 <nikiksen2003@gmail.com>
2023-04-05 23:12:46 +0300

    feat: created function structure

--- 4Homework/BracketsSequence/BracketsSequence/BracketsSequence/Library.fs
+++ 4Homework/BracketsSequence/BracketsSequence/BracketsSequence/Library.fs
@@ -1,5 +1,18 @@
 ﻿namespace BracketsSequence
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System.Collections.Generic
+
+module MyModule =
+    let openBrackets = ['('; '{'; '['; '<']
+    let closeBrackets = [')'; '}'; ']'; '>']
+    
+    let checkBrackets s =
+        let rec checkBracketsRec i (stack: Stack<char>) =
+            if String.length s = i then
+                stack.Count = 0
+            else
+                match s[i] with
+                | c when List.contains c openBrackets -> false
+                | c when List.contains c closeBrackets -> false
+                
+        checkBracketsRec 0 (Stack<char>())
