657e2ec89f1ae3a04e05851d27b2551a65fe343b
niksen111 <nikiksen2003@gmail.com>
2023-04-21 11:47:32 +0300

    feat: writed supermap function + tests for it

---
+++ 1Control/Tasks/Tasks/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace Tasks
+
+module Tasks =
+    let rec supermap l f =
+        match l with
+        | [] -> []
+        | head :: tail -> f head @ supermap tail f
+
+    let rhombus n =
+        n
