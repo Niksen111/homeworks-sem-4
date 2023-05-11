45ced29046a8a558676a5d9fc126f34839bda172
niksen111 <nikiksen2003@gmail.com>
2023-05-11 20:30:00 +0300

    fix: fixed factorial computing

--- 1Homework/1.1Factorial/Factorial/Factorial/Factorial.fs
+++ 1Homework/1.1Factorial/Factorial/Factorial/Factorial.fs
@@ -1,8 +1,10 @@
 module Factorial.Factorial
 
-let rec Factorial x =
-    match x with
-    | _ when x < 0 -> 0
-    | 0 -> 1
-    | _ -> x * Factorial (x - 1)
+let Factorial x =
+    let rec FacRec x1 acc =
+        match x1 with
+        | c when c < 0 -> 0
+        | 0 -> acc
+        | _ -> FacRec (x1 - 1) acc * x1
+    FacRec x 1
         
