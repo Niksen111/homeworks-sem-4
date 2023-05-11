44d6c2f8dff5d273be883e21c696c3dc2ecf9b03
niksen111 <nikiksen2003@gmail.com>
2023-05-11 23:43:01 +0300

    feat: writed tests

--- 4Homework/BracketsSequence/BracketsSequence/BracketsSequenceTests/UnitTest1.fs
+++ 4Homework/BracketsSequence/BracketsSequence/BracketsSequenceTests/UnitTest1.fs
@@ -2,7 +2,17 @@ module BracketsSequenceTests
 
 open NUnit.Framework
 open FsUnit
+open BracketsSequence.MyModule
 
 [<Test>]
 let Test1 () =
+    checkBrackets "(" |> should be False
+    checkBrackets "(}" |> should be False
+    checkBrackets "(()))" |> should be False
+    checkBrackets ")())" |> should be False
+    checkBrackets "({[}])" |> should be False
+    checkBrackets "{[(())]}{[(()}]" |> should be False
+    checkBrackets "" |> should be True
+    checkBrackets "[((){})[]]" |> should be True
+    checkBrackets "(({}[()]))[()]{[()]}({})" |> should be True
     
