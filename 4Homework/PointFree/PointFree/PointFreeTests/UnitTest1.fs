f98ec9b24f1f538b66a5b2570fa96e2e649b8dcd
niksen111 <nikiksen2003@gmail.com>
2023-05-13 13:01:56 +0300

    feat: added new func steps

--- 4Homework/PointFree/PointFree/PointFreeTests/UnitTest1.fs
+++ 4Homework/PointFree/PointFree/PointFreeTests/UnitTest1.fs
@@ -5,9 +5,13 @@ open FsCheck
 open FsUnit
 
 let initialFunc x l = List.map (fun y -> y * x) l
-let func3 x = List.map ((*) x)
+// let func1 x = List.map (fun y -> y * x)
+// let func2 x = List.map (fun y -> (*) x y)
+// let func3 x = List.map ((*) x)
+// let func4 x = List.map << (*) <| x
+let func5 = List.map << (*)
 
-let funcsAreTheSame x l = initialFunc x l = func3 x l
+let funcsAreTheSame x l = initialFunc x l = func5 x l
 
 [<Test>]
 let Test1 () =
@@ -28,5 +32,5 @@ let Test2 () =
     let inputList = [1; 2; 3; 4]
     let expectedOutput = [2; 4; 6; 8]
     initialFunc 2 inputList |> should equal expectedOutput
-    func3 2 inputList |> should equal expectedOutput
+    func5 2 inputList |> should equal expectedOutput
     
