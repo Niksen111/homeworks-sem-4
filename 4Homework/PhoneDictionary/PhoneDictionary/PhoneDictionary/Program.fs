04ef6b681c33f64b62b16bab6cb039f2d7ddf05a
Niksen111 <nikiksen2003@gmail.com>
2023-05-13 15:50:13 +0300

    feat: writed all the functions

--- 4Homework/PhoneDictionary/PhoneDictionary/PhoneDictionary/Program.fs
+++ 4Homework/PhoneDictionary/PhoneDictionary/PhoneDictionary/Program.fs
@@ -18,25 +18,31 @@ module PhoneDictionary =
         writer.Close()
 
     let isPhoneBook (fileName:string) =
-        let lines = File.ReadAllLines(fileName)
-        let rec isPhoneBookRec i =
-            if i = lines.Length then
-                true
-            else
-                let split = lines[i].Split ' '
-                if split.Length <> 2 || not (isPhoneNumber split[0]) then
-                    false
+        if File.Exists fileName then
+            let lines = File.ReadAllLines(fileName)
+            let rec isPhoneBookRec i =
+                if i = lines.Length then
+                    true
                 else
-                    isPhoneBookRec (i + 1)
-        isPhoneBookRec 0
+                    let split = lines[i].Split ' '
+                    if split.Length <> 2 || not (isPhoneNumber split[0]) then
+                        false
+                    else
+                        isPhoneBookRec (i + 1)
+            isPhoneBookRec 0
+        else
+            false
 
     let dictionaryPN = Dictionary<string, string>()
     let dictionaryNP = Dictionary<string, string>()
     
     let add phone name =
-        dictionaryPN.Add (phone, name)
-        dictionaryNP.Add (name, phone)
-        "Success."
+        if isPhoneNumber phone then
+            dictionaryPN.Add (phone, name)
+            dictionaryNP.Add (name, phone)
+            "Success."
+        else
+            "Incorrect phone number."
     let remove phone =
         if not (isPhoneNumber phone) || not (dictionaryPN.ContainsKey phone) then
             "No entry found."
@@ -47,13 +53,39 @@ module PhoneDictionary =
     let findName phone = if dictionaryPN.ContainsKey phone then dictionaryPN.Item(phone) else "No entry found."
     let findPhone name = if dictionaryNP.ContainsKey name then dictionaryNP.Item(name) else "No entry found."
     let print () =
+        if dictionaryPN.Count = 0 then
+            printfn "Phone book is empty."
+        else 
+            for entry in dictionaryPN do
+                printfn "%s %s" entry.Key entry.Value
+    let save (path: string) =
+        use writer = new StreamWriter(path)
         for entry in dictionaryPN do
-            printfn "%s %s" entry.Key entry.Value
-    let save () = ()
-    let read path = ()
+            writer.WriteLine(sprintf "%s %s" entry.Key entry.Value)
+        writer.Close()
+        "Success."
+    let load path =
+        if isPhoneBook path then
+            let lines = File.ReadAllLines(path)
+            let rec readRec i =
+                if i = lines.Length then
+                    ()
+                else
+                    let split = lines[i].Split ' '
+                    dictionaryPN.Add(split[0], split[1])
+                    dictionaryNP.Add(split[1], split[0])
+                    readRec (i + 1)
+            readRec 0
+            "Success."
+        else
+            "File is not a phone book."
+            
     let clear () =
         dictionaryPN.Clear()
         dictionaryNP.Clear()
+        
+    let count () =
+        dictionaryNP.Count
     
     let printHelp () =
         printfn "Commands list:"
@@ -63,9 +95,10 @@ module PhoneDictionary =
         printfn "find -n <phone>     -find name of the contact by the phone number"
         printfn "find -p <name>      -find phone of the contact by the name"
         printfn "print               -print all the contacts in the base"
-        printfn "save                -save the current data to a file"
-        printfn "read <path>         -read the data form the file"
+        printfn "save <path>         -save the current data to a file"
+        printfn "load <path>         -load the data form the file"
         printfn "clear               -erase all existing phone records"
+        printfn "count               -prints the number of entries in the book."
         printfn "help                -command list"
         printfn "<name> must be entered without spaces (using _)"
         printfn "<phone> must be entered in international format"
@@ -100,11 +133,16 @@ module PhoneDictionary =
                 | "-p" -> printfn "%s" <| findPhone chunks[2]
                 | _ -> printErr ()
         | "print" -> if chunks.Length = 1 then print () else printErr ()
-        | "save" -> if chunks.Length = 1 then save () else printErr ()
-        | "read" ->
+        | "save" -> if chunks.Length = 2 then
+                        printfn "%s" (save chunks[1])
+                    else printErr ()
+        | "load" ->
             if chunks.Length = 2 then
-                read chunks[1]
+                printfn "%s" (load chunks[1])
+            else
+                printErr ()
         | "clear" -> if chunks.Length = 1 then clear () else printErr ()
+        | "count" -> if chunks.Length = 1 then printfn "%d" (count()) else printErr ()
         | "help" -> if chunks.Length = 1 then printHelp () else printErr ()
         | _ -> printErr ()
 
