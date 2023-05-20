3d146e461c9d2f83eed0fa585e6c625ad136a25a
Niksen111 <nikiksen2003@gmail.com>
2023-05-20 18:30:38 +0300

    feat: written primeSeq function

---
+++ 2Homework/PrimeNumbers/PrimeNumbers/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace PrimeNumbers
+
+module Numbers =
+    let isPrime x =
+        let rec isPrimeRec x i =
+            if x % i = 0 then
+                false
+            else if i * i > x then
+                true
+            else
+                isPrimeRec x (i + 1)
+        isPrimeRec x 2
+        
+    let primeSequence = Seq.initInfinite id |> Seq.filter isPrime
