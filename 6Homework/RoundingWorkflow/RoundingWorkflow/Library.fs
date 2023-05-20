a586c802b34fa67bfcb0b5b530e6413e8c0e93fd
Niksen111 <nikiksen2003@gmail.com>
2023-05-20 14:29:33 +0300

    feat: created project

---
+++ 6Homework/RoundingWorkflow/RoundingWorkflow/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace RoundingWorkflow
+
+open System
+
+module Workflows =
+    type RoundBuilder(accuracy: int) =
+        member this.Bind(x, f) =
+            f x
+        member this.Return(x) =
+            Math.Round(x |> float, accuracy)
+    let rounding = RoundBuilder(3)
---
+++ 6Homework/RoundingWorkflow/RoundingWorkflowTests/UnitTest1.fs
@@ -0,0 +1,9 @@
+module RoundingWorkflowTests
+
+open NUnit.Framework
+open FsUnit
+open RoundingWorkflow.Workflows
+
+[<Test>]
+let ``Workflow works`` () =
+    ()    
