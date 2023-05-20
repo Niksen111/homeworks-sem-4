f972987e7679facd1d5763bbf2c2f0ab51ba5776
Niksen111 <nikiksen2003@gmail.com>
2023-05-20 16:51:48 +0300

    feat: removed extra functions, added some functions

--- 3Homework/LambdaIntrepreter/LambdaInterpreter/LambdaInterpreter/Library.fs
+++ 3Homework/LambdaIntrepreter/LambdaInterpreter/LambdaInterpreter/Library.fs
@@ -10,45 +10,36 @@ module LambdaInterpreter =
     let fv t =
         let rec fvRec t list =
             match t with
-            | Variable v -> if Seq.contains v list then [] else [ v ] 
+            | Variable v -> if List.contains v list then [] else [ v ] 
             | Applique(t1, t2) -> fvRec t1 list @ fvRec t2 list
             | Abstraction(v, t1) -> fvRec t1 (v :: list)
-        fvRec t [] |> Seq.distinct |> List.ofSeq
+        fvRec t [] |> List.distinct
     let bv t =
         let rec bvRec t =
             match t with
             | Variable _ -> []
             | Applique(t1, t2) -> bvRec t1 @ bvRec t2
             | Abstraction(v, t1) -> v :: bvRec t1
-        bvRec t |> Seq.distinct |> List.ofSeq
-        
-    let getNewFreeVar list =
-        let rec getFreeVarRec c ls cnt =
-            if not(List.contains c ls) then
-                c
-            else if cnt > 256 then
-                char 666
-            else getFreeVarRec (char (int c + 1)) ls (cnt + 1)
-        getFreeVarRec (char 0) list 0
-        
-    let alphaRed var term1 term2 =
-        let rec alphaRedRec var1 term3 =
-            match term3 with
-            | Variable item -> Variable(var1)
-            | Applique(item1, item2) -> Applique(alphaRedRec var1 item1, alphaRedRec var1 item2)
-            | Abstraction(item1, term) -> Abstraction(item1, alphaRedRec var1 term)
-        let freeVar = fv term1 @ fv term2 |> Seq.distinct |> List.ofSeq
-        if not(List.contains var freeVar) then
-            Abstraction(var, term1)
-        else
-            let newVar = getNewFreeVar freeVar
-            Abstraction(newVar, alphaRedRec newVar term1)
-        
+        bvRec t |> List.distinct
+    let getNewFreeVarList (ls: char list) =
+        let allChars = ['a'..'z'] @ ['A'..'Z']
+        allChars |> List.filter (fun c -> not (List.contains c ls))
+    let isValidTerm term =
+        let rec isValidRec term ls =
+            match term with
+            | Variable v -> true
+            | Applique(t1, t2) -> (isValidRec t1 ls) && (isValidRec t2 ls)
+            | Abstraction(v, t) ->
+                if List.contains v ls then
+                    false
+                else
+                    isValidRec t (v::ls)
+        isValidRec term []
     let substitute term v term1 =   
-        let rec substituteRec term2 v term3 =
+        let rec substituteRec term2 term3 =
             match term2 with
             | Variable v1 when v1 = v -> term3
             | Variable v1 -> Variable(v1)
-            | Applique(t1, t2) -> Applique(substituteRec t1 v term3, substituteRec t2 v term3)
-            | Abstraction(v1, t1) -> t1
-        substituteRec term v term1
+            | Applique(t1, t2) -> Applique(substituteRec t1 term3, substituteRec t2 term3)
+            | Abstraction(v1, t1) -> Abstraction(v1, substituteRec t1 term3)
+        substituteRec term term1
