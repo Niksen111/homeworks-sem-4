9161d10b1c753a9fb8bbb90eb8f7ee53c99df002
Niksen111 <nikiksen2003@gmail.com>
2023-05-26 07:30:04 +0300

    feat: written calculations type

---
+++ 6Homework/StringCalculations/StringCalculations/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace StringCalculations
+
+open System
+open Microsoft.FSharp.Core
+
+module Say =
+    let tryParse (str: string) =
+        match Int32.TryParse str with
+        | true, x -> Ok(x)
+        | _ -> Error("Not a number.")
+    
+    type CalculationsBuilder() =
+        member this.Bind(str: string, f) =
+            let parsed = tryParse(str)
+            match parsed with
+            | Ok x -> f x
+            | Error _ -> parsed
+        member this.Result(x: string) =
+            Ok(x)
+            
+    let calculations = CalculationsBuilder()
