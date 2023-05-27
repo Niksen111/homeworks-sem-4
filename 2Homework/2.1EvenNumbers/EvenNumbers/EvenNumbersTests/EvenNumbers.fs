690104f8e8748f6bc0a88d6f4d82e9c60ad30627
Niksen111 <nikiksen2003@gmail.com>
2023-05-27 20:54:13 +0300

    ref: same tests code moved to special function

--- 2Homework/2.1EvenNumbers/EvenNumbers/EvenNumbersTests/EvenNumbers.fs
+++ 2Homework/2.1EvenNumbers/EvenNumbers/EvenNumbersTests/EvenNumbers.fs
@@ -1,6 +1,6 @@
 module EvenNumbersTests
 
-open EvenNumbers
+open EvenNumbers.EvenNumbers
 open NUnit.Framework
 open FsUnit
 open FsCheck
@@ -9,37 +9,34 @@ let simpleList = [ 1 .. 10 ]
 let oddNumbers = [ 1; 3; 5; 7 ]
 let negativeNumbers = [ -10; -6; 10 ]
 
+let checkResults f =
+    simpleList |> f |> should equal 30
+    [] |> f |> should equal 0
+    oddNumbers |> f |> should equal 0
+    negativeNumbers |> f |> should equal -6
+
 [<Test>]
 let ``EvenNumbersFold works`` () =
-    EvenNumbers.EvenNumbersFold(simpleList) |> should equal 30
-    EvenNumbers.EvenNumbersFold([]) |> should equal 0
-    EvenNumbers.EvenNumbersFold(oddNumbers) |> should equal 0
-    EvenNumbers.EvenNumbersFold(negativeNumbers) |> should equal -6
+    EvenNumbersFold |> checkResults
     
 [<Test>]
 let ``EvenNumbersFilter works`` () =
-    EvenNumbers.EvenNumbersFilter(simpleList) |> should equal 30
-    EvenNumbers.EvenNumbersFilter([]) |> should equal 0
-    EvenNumbers.EvenNumbersFilter(oddNumbers) |> should equal 0
-    EvenNumbers.EvenNumbersFilter(negativeNumbers) |> should equal -6
+    EvenNumbersFilter |> checkResults
     
 [<Test>]
 let ``EvenNumbersMap works`` () =
-    EvenNumbers.EvenNumbersMap(simpleList) |> should equal 30
-    EvenNumbers.EvenNumbersMap([]) |> should equal 0
-    EvenNumbers.EvenNumbersMap(oddNumbers) |> should equal 0
-    EvenNumbers.EvenNumbersMap(negativeNumbers) |> should equal -6
+    EvenNumbersMap |> checkResults
     
 [<Test>]
 let ``FsCheck test`` () =
     let ``EvenNumbers Map and Filter are the same`` list =
-        EvenNumbers.EvenNumbersMap list = EvenNumbers.EvenNumbersFilter list
+        EvenNumbersMap list = EvenNumbersFilter list
     Check.QuickThrowOnFailure ``EvenNumbers Map and Filter are the same``
 
     let ``EvenNumbers Map and Fold are the same`` list =
-        EvenNumbers.EvenNumbersMap list = EvenNumbers.EvenNumbersFold list
+        EvenNumbersMap list = EvenNumbersFold list
     Check.QuickThrowOnFailure ``EvenNumbers Map and Fold are the same``
 
     let ``EvenNumbers Fold and Filter are the same`` list =
-        EvenNumbers.EvenNumbersFold list = EvenNumbers.EvenNumbersFilter list
+        EvenNumbersFold list = EvenNumbersFilter list
     Check.QuickThrowOnFailure ``EvenNumbers Fold and Filter are the same``
