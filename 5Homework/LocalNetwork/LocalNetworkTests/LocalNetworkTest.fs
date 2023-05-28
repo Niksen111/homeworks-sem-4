c8bbfad9a2c8cdbe9979260a6761bab4ae1bd766
Niksen111 <nikiksen2003@gmail.com>
2023-05-28 13:04:43 +0300

    fix: fixed tests

--- 5Homework/LocalNetwork/LocalNetworkTests/LocalNetworkTest.fs
+++ 5Homework/LocalNetwork/LocalNetworkTests/LocalNetworkTest.fs
@@ -41,17 +41,17 @@ let ``Only neighboring computers are infected`` () =
         | Mac -> 1.0
     let myNetwork = network chance (System.Random().NextDouble)
     let computers = myNetwork.Computers
-    let after1Step = [|false; true; false; true; false; true; false; false|]
-    let after2Step = [|true; true; true; true; true; true; true; false|]
+    let after1Step = [false; true; false; true; false; true; false; false]
+    let after2Step = [true; true; true; true; true; true; true; false]
         
     myNetwork.Step()
-    for i in 0..7 do
-        computers[i].IsInfected |> should equal after1Step[i]
+    let infected1 = computers |> List.map (fun comp -> comp.IsInfected)
+    infected1 |> should equal after1Step 
     myNetwork.Changeable |> should be True
     
     myNetwork.Step()
-    for i in 0..7 do
-        computers[i].IsInfected |> should equal after2Step[i]
+    let infected2 = computers |> List.map (fun comp -> comp.IsInfected)
+    infected2 |> should equal after2Step 
     myNetwork.Changeable |> should be False
         
 [<Test>]
@@ -62,10 +62,10 @@ let ``Network is not infected if the chance is 0``() =
         | Mac -> 0.0
     let myNetwork = network chance (System.Random().NextDouble)
     let computers = myNetwork.Computers
-    let afterStep = [|false; false; false; true; false; false; false; false|]
+    let afterStep = [false; false; false; true; false; false; false; false]
 
     myNetwork.Changeable |> should be False
     myNetwork.Step()
-    for i in 0..7 do
-        computers[i].IsInfected |> should equal afterStep[i]
+    let infected = computers |> List.map (fun comp -> comp.IsInfected)
+    infected |> should equal afterStep 
     myNetwork.Changeable |> should be False
